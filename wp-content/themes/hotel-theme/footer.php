dbbf5aa9095b139215a2923e104c8b5c8b0bde7c
tboulund <tbmh@easv.dk>
2026-03-09 13:03:51 +0100

    Initial commit

---
+++ wp-content/themes/hotel-theme/footer.php
@@ -0,0 +1,12 @@
+    </main>
+
+    <footer class="footer py-3 bg-dark text-white text-center">
+        &copy; Copyrigth 2026 Hotel CMS. All rights reserved.
+    </footer>
+
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ wp-content/themes/hotel-theme/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel CMS</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/style.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-light bg-light navbar-expand-md">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="<?= get_template_directory_uri(); ?>/images/logo.png" alt="" width="45" height="36" class="d-inline-block align-text-top">
+                    Hotel CMS
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Events</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact us</a>
+                        </li>
+                    </ul>
+                </div>
+                <form class="d-flex navbar-form navbar-right">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <a href="search.html" class="btn btn-outline-success" type="submit">Search</a>
+                </form>
+            </div>
+        </nav>
+    </header>
+
+    <main>
