9d2171206c5b739a12cbd6102be3ae996aaa59b0
olga-pas-easv <olgpas01@easv365.dk>
2026-03-24 11:48:20 +0100

    add contact info

--- wp-content/themes/hotel-theme/index.php
+++ wp-content/themes/hotel-theme/index.php
@@ -3,6 +3,32 @@
         <?php while (have_posts()) : the_post(); ?>
       
 
+        <h3>Contact us</h3>
+            <?php
+            $hotel_name = get_field("hotel_name");
+            $address = get_field("address");
+            $email = get_field("email");
+            $telephone_number = get_field("telephone_number");
+            ?>
+
+            <?php if ($hotel_name) : ?>
+                <p><?= $hotel_name; ?></p>
+            <?php endif; ?>
+
+            <?php if ($address) : ?>
+                <p><?= $address; ?></p>
+            <?php endif; ?>
+
+            <?php if ($email) : ?>
+                <p><?= $email; ?></p>
+            <?php endif; ?>
+            
+            <?php if ($telephone_number) : ?>
+                <p><?= $telephone_number; ?></p>
+            <?php endif; ?>
+
+            
+
 
         <?php endwhile; ?>
     <?php endif ; ?>
