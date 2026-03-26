ef718fda975d1e02d21cd311439e0748c1b70338
olga-pas-easv <olgpas01@easv365.dk>
2026-03-26 14:32:20 +0100

    Add style img, text (hero)

--- wp-content/themes/hotel-theme/template-parts/index-hero.php
+++ wp-content/themes/hotel-theme/template-parts/index-hero.php
@@ -1,14 +1,26 @@
 
 <?php
     $heroFoto = get_field("hero-foto");
-    $heroTitle = get_field("hero-title");
-    $textText = get_field("hero-desctiption");
+    $heroSection = get_field("hero-group");
+    $heroTitle =  $heroSection["hero-title"];
+    $heroText =  $heroSection["hero-desctiption"];
+    $textSection = get_field("text-section");
+    $textSection1 =  $textSection["text-section1"];
+    $textSection2 =  $textSection["text-section2"];
+    $textSection3 =  $textSection["text-section3"];
 ?>
 
-<div class="hero-img" style="background-image: url(<? echo $heroFoto["url"] ?>);" aria-hidden="true"></div>
-<p><?= $heroTitle; ?></p>      
-<p><?= $textText; ?></p>
-
+<div class="hero-img" style="background-image: url(<? echo $heroFoto["url"] ?>);" >
+    <div class="container-welcome">
+        <h1><?= $heroTitle ?></h1>      
+        <p><?= $heroText ?></p>
+    </div>
+</div>
+<div class="text-section">
+     <p><?= $textSection1 ?></p>
+     <p><?= $textSection2 ?></p>
+     <p><?= $textSection3 ?></p>
+</div>
   
               
 
