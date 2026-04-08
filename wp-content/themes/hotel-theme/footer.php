6141181a8b1c0d547736dd3f39938c08878f313a
olga-pas-easv <olgpas01@easv365.dk>
2026-04-08 12:09:11 +0200

    Move contact section to footer
    Relocate the contact-us markup from the front page template into footer.php so contact details appear site-wide. Update get_field calls to pull data from the page set in page_on_front (hotel_name, address, email, telephone_number) and keep the map image in the footer. Remove the duplicate contact block from index.php to avoid redundancy.

--- wp-content/themes/hotel-theme/footer.php
+++ wp-content/themes/hotel-theme/footer.php
@@ -5,6 +5,33 @@
         $map = get_field("map", get_option("page_on_front"));
         ?>
 
+           
+<section class="contact-us">
+            <h3 class="h3-contact">Contact us</h3>
+            <?php
+            $hotel_name = get_field("hotel_name", get_option("page_on_front"));
+            $address = get_field("address", get_option("page_on_front"));
+            $email = get_field("email", get_option("page_on_front"));
+            $telephone_number = get_field("telephone_number", get_option("page_on_front"));
+            ?>
+
+            <?php if ($hotel_name) : ?>
+                <p class="contact-name"><?= $hotel_name; ?></p>
+            <?php endif; ?>
+
+            <?php if ($address) : ?>
+                <p><?= $address; ?></p>
+            <?php endif; ?>
+
+            <?php if ($email) : ?>
+                <p><?= $email; ?></p>
+            <?php endif; ?>
+
+            <?php if ($telephone_number) : ?>
+                <p><?= $telephone_number; ?></p>
+            <?php endif; ?>
+</section>
+
         <img class="map" src="<?= $map["url"] ?>" alt="Map of location">
 
          &copy; Copyright 2026 Hotel CMS. All rights reserved.
--- wp-content/themes/hotel-theme/index.php
+++ wp-content/themes/hotel-theme/index.php
@@ -5,32 +5,6 @@
 <?php get_template_part("template-parts/index", "hero") ?>
 
 <?php get_template_part("template-parts/index", "amenities") ?>
-   
-<section class="contact-us">
-            <h3 class="h3-contact">Contact us</h3>
-            <?php
-            $hotel_name = get_field("hotel_name");
-            $address = get_field("address");
-            $email = get_field("email");
-            $telephone_number = get_field("telephone_number");
-            ?>
-
-            <?php if ($hotel_name) : ?>
-                <p class="contact-name"><?= $hotel_name; ?></p>
-            <?php endif; ?>
-
-            <?php if ($address) : ?>
-                <p><?= $address; ?></p>
-            <?php endif; ?>
-
-            <?php if ($email) : ?>
-                <p><?= $email; ?></p>
-            <?php endif; ?>
-
-            <?php if ($telephone_number) : ?>
-                <p><?= $telephone_number; ?></p>
-            <?php endif; ?>
-</section>
 
         <?php endwhile; ?>
     <?php endif ; ?>
