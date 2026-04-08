d08aafa4f0f61bb788656f854617f0ae35899bcc
Emma-LL <emmlin01@easv365.dk>
2026-04-08 11:49:30 +0200

    Update Secure Custom Fields plugin
    Fix input value handling in acf-field-group.js (use $el.val() instead of input.attr('value')), bump built asset versions and rebuild minified JS/maps. Also update various field classes, AJAX handlers, REST endpoints, composer metadata and vendor autoload files, plus small plugin/readme and theme footer/style tweaks.

--- wp-content/themes/hotel-theme/footer.php
+++ wp-content/themes/hotel-theme/footer.php
@@ -1,8 +1,15 @@
     </main>
 
     <footer class="footer py-3 bg-dark text-white text-center">
-        &copy; Copyrigth 2026 Hotel CMS. All rights reserved.
+         <?php
+        $map = get_field("map", get_option("page_on_front"));
+        ?>
+
+        <img class="map" src="<?= $map["url"] ?>" alt="Map of location">
+
+         &copy; Copyright 2026 Hotel CMS. All rights reserved.
     </footer>
+
 
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
