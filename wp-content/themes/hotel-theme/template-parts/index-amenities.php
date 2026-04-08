6e9ec58fe6b3445bd29c49702cc1bd84c26fc3b7
olga-pas-easv <olgpas01@easv365.dk>
2026-04-08 10:22:59 +0200

    Add scc to amenities

--- wp-content/themes/hotel-theme/template-parts/index-amenities.php
+++ wp-content/themes/hotel-theme/template-parts/index-amenities.php
@@ -3,9 +3,9 @@
     $amenities = get_field("amenities");
 ?>
 
-<div id="amenities" class="amenities">
+<div id="amenities" class="container-amenities">
        <h2>Amenties</h2>      
-       <ul>
+       <ul class="amenities">
             <?php foreach($amenities as $amenity) : ?>
 
                 <?php
